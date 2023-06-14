58d4c111451f320f2f4049888ac89f913a040210
hipig <205270006@qq.com>
2023-06-14 23:21:39 +0800

    添加tabbar

--- src/custom-tab-bar/index.vue
+++ src/custom-tab-bar/index.vue
@@ -1,16 +1,23 @@
 <template>
-    <view class="fixed inset-x-0 bottom-0">
+    <view class="fixed inset-x-0 bottom-0 border-t border-gray-100 border-solid">
         <view class="flex items-start justify-center bg-white">
-            <view v-for="(item, index) in list" class="flex-auto text-center" @click="handleSelect(index)">
-                <view :class="item.icon"></view>
-                <text class="text-xl">{{ item.text }}</text>
+            <view v-for="(item, index) in list" class="flex-auto flex flex-col center py-1.5" :class="{'text-green-500' : selected === index}" @tap="handleSelect(item.pagePath, index)">
+                <view class="text-1.5rem" :class="item.icon"></view>
+                <text class="mt-0.5 text-sm">{{ item.text }}</text>
             </view>
         </view>
     </view>    
 </template>
 
+<script>
+  export default {
+    options: {
+      addGlobalClass: true
+    }
+  }
+</script>
+
 <script setup>
-    import 'uno.css'
     import {switchTab} from '@tarojs/taro'
     import {useTabbarStore} from '../store'
     import { computed } from 'vue'
@@ -28,7 +35,7 @@
         },
         {
             pagePath: '/pages/my/index',
-            icon: 'i-mingcute-menu-fill',
+            icon: 'i-mingcute-more-3-fill',
             text: '更多'
         },
     ]
@@ -39,8 +46,9 @@
         return tabbarStore.selected
     })
 
-    const handleSelect = (index) => {
+    const handleSelect = (url, index) => {
         tabbarStore.select(index)
+        switchTab({url})
     }
 
 </script>
